101bb54c45a42dc1e5543a7ef4db145ece3775e0
toricor <irotoridoritoriyaben@yahoo.co.jp>
2017-10-15 22:19:27 +0900

    jdbc オブジェクトマッピング例

--- src/main/java/com/toricor/demo/App.java
+++ src/main/java/com/toricor/demo/App.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class App implements CommandLineRunner {
@@ -18,9 +22,15 @@ public class App implements CommandLineRunner {
 
 	@Override
 	public void run(String... strings) throws Exception {
-        String sql = "SELECT 1";
-		SqlParameterSource param = new MapSqlParameterSource();
-		Integer result = jdbcTemplate.queryForObject(sql, param, Integer.class);
+        String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
+		SqlParameterSource param = new MapSqlParameterSource()
+				.addValue("id", 1);
+		Customer result = jdbcTemplate.queryForObject(sql, param, new RowMapper<Customer>() {
+            @Override
+            public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return new Customer(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name"));
+            }
+        });
         System.out.println("result = " + result);
 	}
 
