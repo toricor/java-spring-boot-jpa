74ab2c718adc485f0e9efd357ea708ad7622634e
toricor <irotoridoritoriyaben@yahoo.co.jp>
2017-10-21 12:05:00 +0900

    JPAのエンティティを作成

--- src/main/java/com/toricor/demo/domain/Customer.java
+++ src/main/java/com/toricor/demo/domain/Customer.java
@@ -2,11 +2,21 @@ package com.toricor.demo.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity // JPAのEntity
+@Table(name = "customers") // デフォルトではテーブル名 = クラス名
 @Data
-@AllArgsConstructor
+@NoArgsConstructor // JPAの仕様でエンティティクラスには引数のないデフォルトコンストラクタを作る必要がある
+@AllArgsConstructor // JPAとは関係ないが便利なので全フィールドを引数にもつコンストラクタも生成させる
 public class Customer {
+    @Id // エンティティの主キーにつける
+    @GeneratedValue // 主キーがDBで自動採番されることを示す
     private Integer id;
+    @Column(nullable = false)
     private String firstName;
+    @Column(nullable = false)
     private String lastName;
 }
